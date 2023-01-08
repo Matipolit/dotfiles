099727d29e0d277946e061a15a27c7643f7e1706
Mateusz Polito <matipolit@gmail.com>
2023-01-08 21:29:50 +0100

    neovim updates and helix config

--- .config/nvim/lua/servers.lua
+++ .config/nvim/lua/servers.lua
@@ -1,10 +1,19 @@
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.tsserver.setup{
+nvim_lsp = require'lspconfig'
+nvim_lsp.pyright.setup{}
+nvim_lsp.tsserver.setup{
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" }
 }
-require'lspconfig'.sumneko_lua.setup{}
-require('lspconfig').clangd.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.phpactor.setup{}
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.ocamllsp.setup{}
+nvim_lsp.sumneko_lua.setup{}
+nvim_lsp.clangd.setup{}
+nvim_lsp.bashls.setup{}
+nvim_lsp.phpactor.setup{}
+nvim_lsp.rust_analyzer.setup{}
+nvim_lsp.ocamllsp.setup{
+    root_dir = nvim_lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace", "*.ml");
+}
+nvim_lsp.metals.setup{
+    root_dir = nvim_lsp.util.root_pattern('*.scala');
+}
+nvim_lsp.cssls.setup{}
+nvim_lsp.html.setup{}
+nvim_lsp.svelte.setup{}
